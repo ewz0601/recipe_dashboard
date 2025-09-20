7118e92a700c47cea8b43cd1282c8ddab7295421
ewz0601 <ewz0601@gmail.com>
2025-09-20 09:12:39 -0400

    refactoring functions

--- app.py
+++ app.py
@@ -5,8 +5,22 @@ app = Flask(__name__)
 
 init_app(app)
 
-@app.route('/new_recipe', methods = ['GET', 'POST'])
-def new_recipe():
+@app.route('/new_recipe', methods=['GET', 'POST'])
+def create_new_recipe():
+    """
+    Handle the creation of a new recipe.
+
+    GET:
+        Render the new recipe form template.
+
+    POST:
+        Accept form data (title, ingredients, amounts, units, directions),
+        insert a new recipe into the database, and confirm success.
+
+    Returns:
+        str | Response: Success message with link to recipes list (POST),
+        or rendered HTML template for recipe creation (GET).
+    """
     if request.method == 'POST':
         title = request.form['title']
         ingredients = request.form['ingredients']
@@ -25,8 +39,22 @@ def new_recipe():
     
     return render_template('new_recipe.html')
 
+
 @app.route('/')
 def view_recipes():
+    """
+    Display a list of all recipes and details for a selected recipe.
+
+    Retrieves all recipe titles for a dropdown list. If a recipe ID is
+    provided in query parameters, fetch its details.
+
+    Query Parameters:
+        id (int, optional): The ID of the recipe to display in detail.
+
+    Returns:
+        Response: Rendered HTML template with recipe list and optionally
+        selected recipe details.
+    """
     db = get_db()
 
     # Get all recipe names for dropdown
@@ -37,7 +65,7 @@ def view_recipes():
     selected_id = request.args.get('id')
     selected_recipe = None
 
-    # If there an id selected, pull all the columns for that id
+    # If there is an id selected, pull all the columns for that id
     if selected_id:
         cursor = db.execute(
             'SELECT * FROM recipes WHERE id = ?',
@@ -45,9 +73,11 @@ def view_recipes():
         )
         selected_recipe = cursor.fetchone()
 
-    return render_template("recipes.html", 
-                           recipes = recipe_list,
-                           selected = selected_recipe)
+    return render_template(
+        "recipes.html",
+        recipes=recipe_list,
+        selected=selected_recipe
+    )
 
 
 if __name__ == '__main__':
